d7551fb2848c5ae3bfb3b6fd66b8f2699cf1e479
Brian Jones <bjones.tech@icloud.com>
2018-03-04 16:05:08 -0500

    Updated blog post model

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 
 class Post(models.Model):
+    CONTENT_TYPE_CHOICES = (
+        ('DOCUMENT', 'Document'),
+        ('VIDEO', 'Video'),
+    )
+
     title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
     content = models.TextField()
+    content_type = models.CharField(max_length=200, choices=CONTENT_TYPE_CHOICES, default='DOCUMENT')
     published = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
