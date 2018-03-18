23228d3c413d12744aea61ca1b2d2b640bfdb6f2
Brian Jones <bjones.tech@icloud.com>
2018-03-18 11:18:01 -0400

    Added portfolio tag model

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Solution
+from .models import Tag, Solution
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
 
 
 class SolutionAdmin(admin.ModelAdmin):
     list_display = ['title', 'release_date', 'featured']
 
 
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Solution, SolutionAdmin)
--- portfolio/models.py
+++ portfolio/models.py
@@ -2,12 +2,27 @@ from ckeditor.fields import RichTextField
 from django.db import models
 
 
+class Tag(models.Model):
+    CATEGORY_CHOICES = (
+        ('LANGUAGE', 'Language'),
+        ('FRAMEWORK', 'Framework'),
+        ('INTEGRATION', 'Integration'),
+    )
+
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, choices=CATEGORY_CHOICES, default='LANGUAGE')
+
+    def __str__(self):
+        return self.name
+
+
 class Solution(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     content = RichTextField()
     release_date = models.DateField()
     featured = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
