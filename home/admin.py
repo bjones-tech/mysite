1fb7155f78bbe4492e55b8cd0d3c8481d7525ccf
Brian Jones <bjones.tech@icloud.com>
2018-03-25 13:31:05 -0400

    Added home image model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Image
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['url']
+
+
+admin.site.register(Image, ImageAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    file = models.ImageField(upload_to='img/')
+
+    def __str__(self):
+        return self.file.name
+
+    def url(self):
+        return self.file.url
