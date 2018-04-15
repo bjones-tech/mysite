d6eac5f19b5f8af6a90041e1017822caa6995ec1
Brian Jones <bjones.tech@icloud.com>
2018-04-15 15:46:03 -0400

    Updated blog post meta class

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,12 @@ from .models import Post
 
 
 def index(request):
-    published_posts = get_list_or_404(Post.objects.exclude(publish_date=None).order_by('-publish_date'),
-                                      publish_date__lte=timezone.now())
+    published_posts = get_list_or_404(Post.objects.exclude(publish_date=None), publish_date__lte=timezone.now())
+
     return render(request, 'blog/index.html', {'posts': published_posts})
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
+
     return render(request, 'blog/post_detail.html', {'post': post})
