0b7623944b781fd00d09b67e1faf4f8db21b48dc
Brian Jones <bjones.tech@icloud.com>
2018-04-15 14:32:12 -0400

    Added tag list to portfolio index

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 
-from .models import Solution
+from .models import Solution, Tag
 
 
 def index(request):
     solutions = get_list_or_404(Solution.objects.order_by('-featured', 'description_only', '-release_date'))
-    return render(request, 'portfolio/index.html', {'solutions': solutions})
+    tags = get_list_or_404(Tag)
+    return render(request, 'portfolio/index.html', {'solutions': solutions, 'tags': tags})
 
 
 def solution_detail(request, solution_id):
